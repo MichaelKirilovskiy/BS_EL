ea4594db78ab467301d4ed5ba2335f1bcad3c563
MichaelKirilovskiy <michael.kirilovskiy@gmail.com>
2020-07-14 21:32:16 +0300

    adding hero section

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    // slider for background hero_section
+    var swiper = new Swiper('.swiper-container', {
+        
+        autoplay: true,
+        speed: 3000,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          dynamicBullets: true,
+        },
+      });
+
+
+
+});
